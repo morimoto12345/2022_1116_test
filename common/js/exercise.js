ddd095d270c360cf2dde03447480621200a21640
yajima_chihiro <yajima@lyzon.co.jp>
2023-01-11 16:26:58 +0900

    fix:conflict

--- common/js/exercise.js
+++ common/js/exercise.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-// 今日の日にちが偶数だったら「こんにちは！いい天気ですね」にし、
-// 奇数だったら「こんにちは！寒いですね」にしてください
-// functionを一回は使おう
-
-document.getElementById('text').innerHTML = '寒いですね'; //DOM操作がうまくいかない
-=======
 // 今日の日にちが偶数だったら「こんにちは！いい天気ですね」にし、
 // 奇数だったら「こんにちは！寒いですね」にしてください
 // functionを一回は使おう
@@ -18,4 +11,3 @@ var changeText = function(value){
     }
 };
  changeText(now.getDate());
->>>>>>> main
